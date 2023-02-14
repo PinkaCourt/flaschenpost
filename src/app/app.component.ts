dddb585cb6fb374f220d14e5e5d6ac0420162d74
Polina Zharkova <polina.zharkova@x5.ru>
2023-02-14 11:15:25 +0300

    правим стили для всего проекта, добавляем все в корень

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProduct } from './models/product';
+import { ProductsService } from './services/products.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-flaschenpost';
+export class AppComponent implements OnInit {
+  title = 'flaschenpost.de';
+
+  products$: Observable<IProduct[]>;
+  sortedByAsc = true;
+  detailView = true;
+  filter = false;
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.products$ = this.productsService.getAll();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product/product.component';
+import { SorterProductsPipe } from './pipes/sorter-products.pipe';
+import { FilterProductsPipe } from './pipes/filter-products.pipe';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    SorterProductsPipe,
+    FilterProductsPipe,
+  ],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
