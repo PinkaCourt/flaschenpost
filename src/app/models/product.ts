4e00abcdf34797d54cb6c5fc596868cb25a9a604
Polina Zharkova <polina.zharkova@x5.ru>
2023-02-14 11:14:37 +0300

    выносим сервисы и интерфейсы для товаров в отдельные папки

---
+++ src/app/models/product.ts
@@ -0,0 +1,16 @@
+interface IAarticles {
+  id: number;
+  shortDescription: string;
+  price: number;
+  unit: string;
+  pricePerUnitText: string;
+  image: string;
+}
+
+export interface IProduct {
+  id: number;
+  brandName: string;
+  name: string;
+  descriptionText?: string;
+  articles: IAarticles[];
+}
